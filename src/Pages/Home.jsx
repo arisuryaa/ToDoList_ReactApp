7eed8938df1190666c5bf82856b4258b0cf405f2
arisuryaa <arisurya228@gmail.com>
2023-11-11 15:32:25 +0800

    akhirnyaaa menambah fitur edit data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,30 +2,21 @@ import React from "react";
 import Button from "../Elements/Button";
 import Form from "../Fragments/Form";
 import { useState } from "react";
-import Edit from "../Fragments/Edit";
 
 const Home = () => {
   const [database, setDatabase] = useState([
     {
       id: 1,
-      text: "aku akan jadi kaya",
-    },
-
-    {
-      id: 2,
-      text: "aku akan buktikan!",
-    },
-    {
-      id: 3,
       text: "Tunggu aku!!!",
     },
   ]);
-
   const [openModal, setOpenModal] = useState(false);
+  const [data, setData] = useState(0);
+  const [values, setValues] = useState("");
 
   const delData = (id) => {
     const yakin = confirm("yakin?");
-    return yakin ? setDatabase(database.filter((e) => e.id !== id)) : "";
+    return yakin ? setDatabase(database.filter((e) => e.id !== id)) : null;
   };
 
   const addData = (datas) => {
@@ -38,47 +29,73 @@ const Home = () => {
     return datas.length < 1 ? false : setDatabase([...database, newData]);
   };
 
-  const HandleOnOpen = () => {
+  const HandleOnOpen = (id) => {
+    setData(id);
+    console.log(id);
     setOpenModal(!openModal);
   };
 
+  const editData = (text) => {
+    const id = data;
+    const newDatas = {
+      id: id,
+      text: text,
+    };
+    const updateData = database.map((item) => {
+      if (item.id === id) {
+        return newDatas;
+      }
+      return item;
+    });
+    setDatabase(updateData);
+    setOpenModal(!openModal);
+  };
+
+  const HandleOnChanges = (e) => {
+    setValues(e.target.value);
+  };
+
   return (
-    <div className="w-full h-screen flex flex-col bg-slate-300">
-      <div className="flex items-center w-full flex-col pt-5">
+    <div className="flex flex-col w-full h-screen bg-slate-300">
+      <div className="flex flex-col items-center w-full pt-5">
         <h1 className="text-4xl font-semibold text-slate-500">To Do List Item</h1>
-        <h1 className="text-sm pt-3 font-semibold text-slate-500">by Dewa</h1>
+        <h1 className="pt-3 text-sm font-semibold text-slate-500">by Dewa</h1>
       </div>
-      <div className="mt-5 flex flex-col flex-wrap gap-5 justify-center items-center w-full h-full">
+      <div className="flex flex-col flex-wrap items-center justify-center w-full h-full gap-5 mt-5">
         {database.map((e, i) => (
-          <div className="w-4/5 lg:w-1/2 bg-white h-fit py-3 rounded-md flex justify-between px-4 items-center" key={i}>
-            <h2>{e.text}</h2>
-            <div className="flex gap-2">
-              <button className="bg-blue-500 text-white px-5 py-1 rounded-md" onClick={HandleOnOpen}>
-                Edit
-              </button>
-              <Button onDel={() => delData(e.id)}>Delete</Button>
-            </div>
-          </div>
-        ))}
-        <Form submit={addData}></Form>
-      </div>
-      {openModal && (
-        <div className="fixed top-0 left-0 h-screen w-screen z-10 flex items-center justify-center">
-          <div className="bg-black h-full w-full opacity-50 absolute top-0 left-0 z-10"></div>
-          <div className="bg-white w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 p-8">
-            <div className="flex flex-col gap-5 items-center">
-              <h1 className="font-bold text-2xl">Edit Data</h1>
-              <input type="text" className="bg-slate-600 text-white w-2/3 h-10 rounded-md placeholder:text-white px-5" placeholder="Masukkan data" />
-              <div className="flex gap-5">
-                <Button variant="bg-blue-500 text-white px-5 py-1 rounded-md">Submit</Button>
-                <button className=" bg-red-500 text-white px-5 py-1 rounded-md" onClick={HandleOnOpen}>
-                  Cancel
+          <div key={i} className="flex justify-center w-full ">
+            <div className="flex items-center justify-between w-full px-4 py-3 bg-white rounded-md lg:w-1/2 h-fit">
+              <h2>{e.text}</h2>
+              <div className="flex gap-2">
+                <button className="px-5 py-1 text-white bg-blue-500 rounded-md" onClick={() => HandleOnOpen(e.id)}>
+                  Edit
                 </button>
+                <Button onDel={() => delData(e.id)}>Delete</Button>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        ))}
+        {openModal && (
+          <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-screen h-screen">
+            <div className="absolute top-0 left-0 z-10 w-full h-full bg-black opacity-50"></div>
+            <div className="absolute z-20 w-1/2 p-8 transform -translate-x-1/2 -translate-y-1/2 bg-white top-1/2 left-1/2">
+              <div className="flex flex-col items-center gap-5">
+                <h1 className="text-2xl font-bold">Edit Data</h1>
+                <input type="text" className="w-2/3 h-10 px-5 text-white rounded-md bg-slate-600 placeholder:text-white" placeholder="Masukkan data" onChange={HandleOnChanges} />
+                <div className="flex gap-5">
+                  <button className="px-5 py-1 text-white bg-blue-500 rounded-md" onClick={() => editData(values)}>
+                    Submit
+                  </button>
+                  <button className="px-5 py-1 text-white bg-red-500 rounded-md" onClick={() => setOpenModal(!openModal)}>
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        <Form submit={addData}></Form>
+      </div>
     </div>
   );
 };
